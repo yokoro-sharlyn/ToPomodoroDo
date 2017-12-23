cdc5815669345afc405439c8750b34cb036eea87
atopilskii <atopilskii@ooyala.com>
2017-12-23 16:53:58 +0300

    Add database to get list of pomodoro

--- app/src/main/java/com/topomodorodo/alexa/topomodorodo/view/ListPomodoroActivity.java
+++ app/src/main/java/com/topomodorodo/alexa/topomodorodo/view/ListPomodoroActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.topomodorodo.alexa.topomodorodo.R;
+import com.topomodorodo.alexa.topomodorodo.database.PomodoroDatabase;
+import com.topomodorodo.alexa.topomodorodo.pomodoro.state.PomodoroStateSwitcher;
 import com.topomodorodo.alexa.topomodorodo.view.adapter.PomodoroAdapter;
 
 import java.util.LinkedList;
@@ -14,14 +16,18 @@ import java.util.LinkedList;
 public class ListPomodoroActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    PomodoroStateSwitcher pomodoroStateSwitcher;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pomodoro_list);
+        PomodoroDatabase pomodoroDatabase = new PomodoroDatabase(getApplicationContext());
+
         recyclerView = findViewById(R.id.pomodoro_recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        recyclerView.setAdapter(new PomodoroAdapter(new LinkedList<>()));
+        recyclerView.setAdapter(new PomodoroAdapter(pomodoroDatabase.getAllPomodoro()));
 
     }
 
